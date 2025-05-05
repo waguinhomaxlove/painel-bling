d2c2afb687d14097a8043fe461b21f69f8e30c9a
waguinhomaxlove <senhorhardware@gmail.com>
2025-05-05 15:42:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,6 @@ def respostas():
     return render_template("respostas.html", texto=texto)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
